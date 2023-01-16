9efb83042c5bd4a7e1c349b1317e12b37106bdde
GraceMurigi <gracemurigi95@gmail.com>
2023-01-16 14:29:45 +0300

    First render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import Model from './components/Model.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1 style={{ color: 'black', textAlign: 'center' }}>
+          3D MODEL LOADING DEMO
+        </h1>
       </header>
-    </div>
+      <Canvas
+        camera={{ position: [2, 0, 12.25], fov: 15 }}
+        style={{
+          backgroundColor: '#111a21',
+          width: '100vw',
+          height: '90vh',
+        }}
+      >
+        <ambientLight intensity={1.25} />
+        <ambientLight intensity={0.1} />
+        <directionalLight intensity={0.4} />
+        <Suspense fallback={null}>
+          <Model position={[0.025, -0.9, 0]} /> /* highlight-line */
+        </Suspense>
+        <OrbitControls />
+      </Canvas>
+    </>
   );
 }
 
